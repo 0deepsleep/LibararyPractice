48cff477b55b76a2b07e63bc7dfcd5ba168436c7
0deepsleep <0deepsleep@gmail.com>
2020-04-21 22:18:33 +0900

    인터넷상 이미지 glide 통해 가져오기 http 이미지주소도 이제 보여짐

--- app/src/main/java/dasdsa/sdn/libararypractice/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/libararypractice/MainActivity.kt
@@ -17,5 +17,7 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setValue()
+        setupEvents()
     }
 }
