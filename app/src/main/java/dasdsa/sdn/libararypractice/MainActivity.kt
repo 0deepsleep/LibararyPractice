c1d9d824789fe0659a1a5916a733388d13ef6a4d
0deepsleep <0deepsleep@gmail.com>
2020-04-21 21:44:33 +0900

    인터넷상 이미지 glide 통해 가져오기 (권한 때매 아직 구동 안함)

--- app/src/main/java/dasdsa/sdn/libararypractice/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/libararypractice/MainActivity.kt
@@ -2,6 +2,8 @@ package dasdsa.sdn.libararypractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun setupEvents() {
@@ -9,7 +11,7 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValue() {
-
+        Glide.with(mContext).load("https://pbs.twimg.com/profile_images/1175388672281075712/hJq4CGkQ_400x400.jpg").into(profileImg)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
