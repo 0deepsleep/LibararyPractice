1e2e72358721b83047f8701e268e5caf0caf3ed8
0deepsleep <0deepsleep@gmail.com>
2020-04-22 20:00:00 +0900

    전화 걸기 기능 작성 - TedPermission 이용해 권한 획득

--- app/src/main/java/dasdsa/sdn/libararypractice/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/libararypractice/MainActivity.kt
@@ -1,12 +1,45 @@
 package dasdsa.sdn.libararypractice
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextWatcher
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun setupEvents() {
+
+        callBtn.setOnClickListener {
+            val permissionListener = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    // 사용자가 권한을 승인해준 상태
+                    val uri = Uri.parse("tel:01063313939")
+                    val myIntent = Intent(Intent.ACTION_CALL,uri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    // 사용자가 권한 승인을 거부한 상태
+                    Toast.makeText(mContext,"권한 허용 필요",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+                .setPermissionListener(permissionListener)
+                .setDeniedMessage("설정 > 권한에서 권한을 바꿔주세요.")
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+
+
+        }
 
     }
 
